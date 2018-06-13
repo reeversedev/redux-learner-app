a326e27aafa843ea8f947c635fcb65889062d7e1
root <gogia.prateek@hotmail.com>
2018-06-13 12:23:56 +0530

    Added connect function from react-redux to connect react with redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-
-export default class BookList extends Component {
+import {connect} from 'react-redux';
+ 
+class BookList extends Component {
   renderList = () => {
       return this.props.books.map((book) => {
           return(
@@ -18,3 +19,12 @@ export default class BookList extends Component {
         )
   }
 }
+function mapStateToProps(state) {
+    // Whatever is returned will show up as props
+    // inside of BookList
+    return {
+        books: state.books 
+    };
+}
+
+export default connect(mapStateToProps)(BookList)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import {combineReducers} from 'react-redux';
+import {combineReducers} from 'redux';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
